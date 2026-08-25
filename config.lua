2420efd2d8a6ea4b2e63e13a19b8b8a957c5a8ea
Alexandre4803 <155264646+Alexandre171172@users.noreply.github.com>
2026-08-25 17:08:32 +0200

    Add SCRAM configuration

---
+++ config.lua
@@ -0,0 +1,10 @@
+-- Configuration du systeme SCRAM
+return {
+  leverSide = "left",
+  cardSide = "right",
+  scramOutputSide = "back",
+  monitorSide = nil, -- nil = premier moniteur trouve
+  cardTimeout = 15,
+  updateInterval = 0.1,
+  textScale = 0.8
+}
